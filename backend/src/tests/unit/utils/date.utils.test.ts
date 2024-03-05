86a6804bbaf520c104fd6ec26f316cdae8dce626
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-05 01:28:06 -0300

    test: date convert

---
+++ backend/src/tests/unit/utils/date.utils.test.ts
@@ -0,0 +1,25 @@
+import { convertToDate } from "../../../utils/date.utils"
+
+describe("Convert to Date", () => {
+    it('should convet a valid date string to a Date object', () => {
+        const dateString = '20240511'
+
+        const result = convertToDate(dateString)
+
+        expect(result.getUTCFullYear()).toBe(2024)
+        expect(result.getUTCMonth()).toBe(4)
+        expect(result.getUTCDate()).toBe(11)
+    })
+
+    it('should throw an error for an empty date string', () => {
+        const dateString = ''
+
+        expect(() => convertToDate(dateString)).toThrow('A string de data está vazia.')
+    })
+
+    it('should throw an error for an invalid date string', () => {
+        const dateString = '20240231'
+
+        expect(() => convertToDate(dateString)).toThrow('Data inválida.')
+    })
+})
