abd93f955fe0922055ac253bb53408b9e2f841b8
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2024-03-09 12:07:23 -0300

    feat: Port no .env

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,3 +1,3 @@
 import { app } from './app'
 
-app.listen(3000, () => console.log(`Server on at port 3000`))
+app.listen(process.env.PORT, () => console.log(`Server on at port ${process.env.PORT}`))
